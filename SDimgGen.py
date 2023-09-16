96840ab33ebb7bc19edf1b7f5d66c50fe052c9b0
sagetgll <sagetgll@gmail.com>
2023-09-16 01:20:46 -0400

    working example/prototype of image generation

--- SDimgGen.py
+++ SDimgGen.py
@@ -6,8 +6,11 @@ from PIL import Image, PngImagePlugin
 
 url = "http://127.0.0.1:7860"
 
+#PROMPT FROM RIFFUSION OR TWITCH CHAT
+prompt = "soft jazz" 
+
 payload = {
-    "prompt": "visual representation of lofi music",
+    "prompt": "visual representation of "+ prompt +" music",
     "steps": 20
 }
 
@@ -15,14 +18,18 @@ response = requests.post(url=f'{url}/sdapi/v1/txt2img', json=payload)
 
 r = response.json()
 
-for i in r['images']:
-    image = Image.open(io.BytesIO(base64.b64decode(i.split(",",1)[0])))
+print(r) #just testing
 
-    png_payload = {
-        "image": "data:image/png;base64," + i
-    }
-    response2 = requests.post(url=f'{url}/sdapi/v1/png-info', json=png_payload)
+#taking base64 string and converting it to a useable image
+image = Image.open(io.BytesIO(base64.b64decode(i.split(",",1)[0])))
 
-    pnginfo = PngImagePlugin.PngInfo()
-    pnginfo.add_text("parameters", response2.json().get("info"))
-    image.save('output.png', pnginfo=pnginfo)
+png_payload = {
+    "image": "data:image/png;base64," + i
+}
+response2 = requests.post(url=f'{url}/sdapi/v1/png-info', json=png_payload)
+
+pnginfo = PngImagePlugin.PngInfo()
+pnginfo.add_text("parameters", response2.json().get("info"))
+
+#saving image to local for now. have to get this into OBS automatically somehow
+image.save('output.png', pnginfo=pnginfo)
